0eaf2d9dd9f9fdf02bdcef9f69c6f33aa53ae5de
Michael White <mwhite7372@gmail.com>
2020-10-29 17:31:35 -0500

    Added user authentication

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,13 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/Signup">Signup</router-link> |
+      <router-link to="/Login">Login</router-link> |
+      <router-link to="/Logout">Logout</router-link> |
+
+      <!-- <router-link to="/about">About</router-link> -->
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
