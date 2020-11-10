342355205fe313f588596abc3abffb700d885368
Michael White <mwhite7372@gmail.com>
2020-11-10 17:43:48 -0600

    Added working dropdown to select year for team show

--- src/views/TeamsShow.vue
+++ src/views/TeamsShow.vue
@@ -3,19 +3,14 @@
     <!-- <h1>Show Team</h1> -->
 
     <!-- Dropdown select example -->
-    <!-- <select class="form-control" @change="getTeamSeason($event)">
-      <option value="" selected disabled>Choose Season</option>
-      <option
-        v-for="season in seasons"
-        :value="jobTitle.id"
-        :key="jobTitle.id"
-        >{{ jobTitle.name }}</option
-      >
-    </select> -->
-    <!-- <br /><br />
+    <select class="form-control" @change="changeSeasons($event)">
+      <option value="" selected disabled>Choose</option>
+      <option v-for="season in seasons">{{ season }}</option>
+    </select>
+    <br /><br />
     <p>
-      <span>Selected job title: {{ selectedJobTitle }}</span>
-    </p> -->
+      <span>Selected Season: {{ selectedSeason }}</span>
+    </p>
     <!-- Dropdown select example -->
 
     <h1>{{ team.name }}</h1>
@@ -54,23 +49,28 @@ export default {
     return {
       team: {},
       selectedSeason: "latest",
-      jobTitles: [
-        { name: "Current" },
-        { name: "2019-2020-regular" },
-        { name: "2019-2020-Playoffs" },
-        { name: "2018-2019-regular" },
-      ],
+      seasons: ["Current", "2020-regular", "2019-regular", "2018-Regular"],
     };
   },
   created: function() {
-    // axios.get(`/api/teams/${this.$route.params.id}`).then((response) => {
-    //   console.log(response.data);
-    //   this.team = response.data;
-    // });
-    this.getTeamSeason();
+    axios
+      .get(`/api/teams/${this.$route.params.id}`, {
+        params: {
+          season: this.selectedSeason,
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        this.team = response.data;
+      });
+    // this.changeSeasons();
   },
   methods: {
-    getTeamSeason: function() {
+    changeSeasons(event) {
+      this.selectedSeason =
+        event.target.options[event.target.options.selectedIndex].text;
+      // },
+      // getTeamSeason: function() {
       axios
         .get(`/api/teams/${this.$route.params.id}`, {
           params: {
@@ -85,32 +85,3 @@ export default {
   },
 };
 </script>
-
------>Example for Season Select below <--------
-
-<div id="app">
-  <select class="form-control" @change="changeJobTitle($event)">
-    <option value="" selected disabled>Choose</option>
-    <option v-for="jobTitle in jobTitles" :value="jobTitle.id" :key="jobTitle.id">{{ jobTitle.name }}</option>
-  </select>
-  <br><br>
-  <p><span>Selected job title: {{ selectedJobTitle  }}</span></p>
-</div>
-
-new Vue({ el: "#app", data: { jobTitles: [ { name: "Product designer", id: 1 },
-{ name: "Full-stack developer", id: 2 }, { name: "Product manager", id: 3 }, {
-name: "Senior front-end developer", id: 4 } ], selectedJobTitle: null },
-methods: { changeJobTitle (event) { this.selectedJobTitle =
-event.target.options[event.target.options.selectedIndex].text } } }) ----> end
-of season else method example <-------
-
-<label for="seasonView">Choose a season:</label>
-
-<select name="seasonView" id="seasonView">
-      <option value="latest">Latest</option>
-      <option value="current">Current</option>
-      <option value="2019-2020-regular">2019-2020-regular</option>
-      <option value="2019-2020-playoffs">2019-2020-playoffs</option>
-      <option value="2018-2019-regular">2019-2020-regular</option>
-      <option value="2018-2019-playoffs">2019-2020-regular</option>
-    </select>
---
+++ temp2.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <select class="form-control" @change="changeSeasons($event)">
+      <option value="" selected disabled>Choose</option>
+      <option v-for="season in seasons">{{ season }}</option>
+    </select>
+    <br /><br />
+    <p>
+      <span>Selected Season: {{ selectedSeason }}</span>
+    </p>
+  </div>
+</template>
+
+<script>
+new Vue({
+  el: "#app",
+  data: {
+    seasons: [
+      { name: "Current" },
+      { name: "2019-2020-regular" },
+      { name: "2019-2020-Playoffs" },
+      { name: "2018-2019-regular" },
+    ],
+    selectedSeason: "latest",
+  },
+  methods: {
+    changeSeasons(event) {
+      this.selectedSeason =
+        event.target.options[event.target.options.selectedIndex].text;
+    },
+  },
+});
+</script>
