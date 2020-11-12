46933082566d54c79928f09e94e70c08701d4679
Michael White <mwhite7372@gmail.com>
2020-11-12 13:09:49 -0600

    Initial commit with new HTML theme implementations started

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,293 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link v-if="!isLoggedIn()" to="/Login">Login </router-link>|
-      <router-link v-if="!isLoggedIn()" to="/Signup">New User</router-link> |
-      <router-link v-if="isLoggedIn()" to="/user_teams">My Teams</router-link> |
-      <router-link to="/teams">All Teams</router-link> |
-      <!-- <router-link to="/teams/:id">Show Team</router-link> | -->
-      <router-link v-if="isLoggedIn()" to="/users/:id">User Info |</router-link>
-      <router-link v-if="isLoggedIn()" to="/Logout"> Logout</router-link>
-    </div>
+    <!-- Top Bar -->
+    <section class="top-bar" id="btt">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-9">
+            <div class="bar-left">
+              <ul class="list-unstyled list-inline">
+                <li class="list-inline-item">
+                  <i class="fa fa-calendar"></i>Sunday, Feb 11, 2019
+                </li>
+                <li class="list-inline-item">
+                  <i class="fa fa-cloud"></i>New York, 19°C
+                </li>
+                <li class="list-inline-item"><a href="">Advertise</a></li>
+                <li class="list-inline-item"><a href="">Write Us</a></li>
+                <li class="list-inline-item"><a href="">About</a></li>
+                <li class="list-inline-item"><a href="">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="bar-social text-right">
+              <ul class="list-unstyled list-inline">
+                <li class="list-inline-item">
+                  <a href=""><i class="fa fa-facebook"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href=""><i class="fa fa-twitter"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href=""><i class="fa fa-linkedin"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href=""><i class="fa fa-google-plus"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href=""><i class="fa fa-rss"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href=""><i class="fa fa-youtube"></i></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Top Bar -->
+    <!-- Menu Area -->
+    <section class="menu-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-9">
+            <div class="main-menu">
+              <ul class="list-unstyled list-inline">
+                <li class="list-inline-item">
+                  <router-link to="/">Home</router-link>
+                </li>
+                <li v-if="isLoggedIn()" class="list-inline-item">
+                  <router-link to="/user_teams">My Teams</router-link>
+                </li>
+                <li class="list-inline-item">
+                  <router-link to="/teams">All Teams</router-link>
+                </li>
+                <li v-if="!isLoggedIn()" class="list-inline-item">
+                  <router-link to="/Login">Login </router-link>
+                </li>
+                <li v-if="!isLoggedIn()" class="list-inline-item">
+                  <router-link to="/Signup">New User</router-link>
+                </li>
+                <li v-if="isLoggedIn()" class="list-inline-item">
+                  <router-link to="/Logout"> Logout</router-link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="clock text-right">
+              <span id="dg-clock"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Menu Area -->
+    <!-- Mobile Menu -->
+    <section class="mobile-menu-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="mobile-menu">
+              <nav id="dropdown">
+                <a href=""><img src="images/f-logo.png" alt=""/></a>
+                <ul class="list-unstyled">
+                  <li>
+                    <a>Home</a>
+                    <ul class="list-unstyled">
+                      <li><a href="index.html">Home Style 1</a></li>
+                      <li><a href="02-home-two.html">Home Style 2</a></li>
+                      <li><a href="03-home-three.html">Home Style 3</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a>Pages</a>
+                    <ul class="list-unstyled">
+                      <li><a href="04-about-us.html">About Us</a></li>
+                      <li>
+                        <a href="05-category-one.html">Category Style 1</a>
+                      </li>
+                      <li>
+                        <a href="06-category-two.html">Category Style 2</a>
+                      </li>
+                      <li>
+                        <a href="07-category-three.html">Category Style 3</a>
+                      </li>
+                      <li>
+                        <a href="08-category-four.html">Category Style 4</a>
+                      </li>
+                      <li>
+                        <a href="09-news-details-one.html">Single News 1</a>
+                      </li>
+                      <li>
+                        <a href="10-news-details-two.html">Single News 2</a>
+                      </li>
+                      <li><a href="12-faq.html">Faq</a></li>
+                      <li><a href="11-contact.html">Contact</a></li>
+                      <li><a href="13-404.html">404</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Politics</a></li>
+                  <li><a href="#">Business</a></li>
+                  <li><a href="#">Sports</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Mobile Menu -->
+
     <router-view />
+    <!-- Footer Area -->
+    <footer class="footer-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="f-about">
+              <img src="images/f-logo.png" alt="" />
+              <p>
+                Lorem ipsum dolor sit amet, consectet adipisicing elit. Saepe
+                porro neque a nam nulla quos atque.
+              </p>
+              <ul class="list-unstyled f-contact">
+                <li>
+                  <i class="fa fa-map-marker"></i>795 South Park Avenue, CA
+                  94107
+                </li>
+                <li><i class="fa fa-envelope"></i>enquery@domain.com</li>
+                <li><i class="fa fa-phone"></i>+1 908 875 7678</li>
+              </ul>
+              <ul class="list-unstyled list-inline f-social">
+                <li class="list-inline-item">
+                  <a href=""><i class="fa fa-facebook"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href=""><i class="fa fa-twitter"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href=""><i class="fa fa-linkedin"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href=""><i class="fa fa-google-plus"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href=""><i class="fa fa-rss"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href=""><i class="fa fa-youtube"></i></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="f-service">
+              <div class="sec-title">
+                <h5>Services</h5>
+              </div>
+              <ul class="list-unstyled service-list">
+                <li>
+                  <a href=""><i class="fa fa-angle-right"></i>Advertisement</a>
+                </li>
+                <li>
+                  <a href=""><i class="fa fa-angle-right"></i>Work for us</a>
+                </li>
+                <li>
+                  <a href=""><i class="fa fa-angle-right"></i>Post a deal</a>
+                </li>
+                <li>
+                  <a href=""><i class="fa fa-angle-right"></i>Agreements</a>
+                </li>
+                <li>
+                  <a href=""
+                    ><i class="fa fa-angle-right"></i>Sms subscription</a
+                  >
+                </li>
+                <li>
+                  <a href=""><i class="fa fa-angle-right"></i>Conference</a>
+                </li>
+                <li>
+                  <a href=""><i class="fa fa-angle-right"></i>Donate us</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="f-post">
+              <div class="sec-title">
+                <h5>Latest Post</h5>
+              </div>
+              <div class="post-box d-flex">
+                <div class="post-img">
+                  <a href=""><img src="images/post1.jpg" alt=""/></a>
+                </div>
+                <div class="post-con">
+                  <p><a href="#">Lorem ipsum dolor sit...</a></p>
+                  <span>Feb 11, 2019</span>
+                </div>
+              </div>
+              <div class="post-box d-flex">
+                <div class="post-img">
+                  <a href=""><img src="images/post2.jpg" alt=""/></a>
+                </div>
+                <div class="post-con">
+                  <p><a href="#">Lorem ipsum dolor sit...</a></p>
+                  <span>Feb 11, 2019</span>
+                </div>
+              </div>
+              <div class="post-box d-flex">
+                <div class="post-img">
+                  <a href=""><img src="images/post3.jpg" alt=""/></a>
+                </div>
+                <div class="post-con">
+                  <p><a href="#">Lorem ipsum dolor sit...</a></p>
+                  <span>Feb 11, 2019</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="f-newsltr">
+              <div class="sec-title">
+                <h5>Newsletter</h5>
+              </div>
+              <form class="news-letter" action="#">
+                <input
+                  type="text"
+                  name="name"
+                  value=""
+                  placeholder="Your name"
+                />
+                <input
+                  type="text"
+                  name="email"
+                  value=""
+                  placeholder="Your email address"
+                />
+                <button type="button" name="button">Register</button>
+              </form>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="f-menu d-flex justify-content-between">
+              <a href="">About Us</a>
+              <a href="">Archives</a>
+              <a href="">Terms & Condition</a>
+              <a href="">Privacy Policy</a>
+              <a href="">Rss</a>
+              <a href="">Apps</a>
+              <a href="">Contact</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- End Footer Area -->
   </div>
 </template>
-
-import func from "../vue-temp/vue-editor-bridge";
 
 <script>
 export default {
@@ -28,25 +301,3 @@ export default {
   },
 };
 </script>
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/views/TeamsShow.vue
+++ src/views/TeamsShow.vue
@@ -1,68 +1,135 @@
 <template>
   <div class="teams-show">
-    <!-- <h1>Show Team</h1> -->
-
-    <!-- Dropdown select for season -->
-    <select class="form-control" @change="changeSeasons($event)">
-      <option value="" selected disabled>Choose Season</option>
-      <option v-for="season in seasons">{{ season }}</option>
-    </select>
-    <!-- Free version of API only allows for selection of two seasons.  If I ever go with paid version then will make changes to add playoffs and more historical season data -->
-    <br /><br />
-
-    <h4>Season Highlights shown: {{ selectedSeason }}</h4>
-
-    <!-- Dropdown select  -->
-
-    <h1>{{ team.name }}</h1>
-    <img :src="team.official_logo" alt="" />
-
-    <div>
-      <!-- Added a NCAAF if loop for future NCAA implementation -->
-      <div v-if="team.league == 'NCAA'">
-        <h5>Conference: {{ team.conference_name }}</h5>
-        <h5>Games Played: {{ team.games_played }}</h5>
-        <h5>Games Won: {{ team.games_won }}</h5>
-        <h5>Games Lost: {{ team.games_lost }}</h5>
-        <h5>Conference Wins: {{ team.conference_wins }}</h5>
-        <h5>Conference Losses: {{ team.conference }}</h5>
-      </div>
-      <div v-else>
-        <h5>Conference: {{ team.conference_name }}</h5>
-        <h5>Division: {{ team.division_name }}</h5>
-        <h5>Home Venue: {{ team.team_venue }}</h5>
-        <h5>Games Played: {{ team.games_played }}</h5>
-        <h5>Games Won: {{ team.games_won }}</h5>
-        <h5>Games Lost: {{ team.games_lost }}</h5>
-        <!-- NHL had unique stats -->
-        <div v-if="team.league == 'NHL'">
-          <h5>Overtime Wins: {{ team.overtime_wins }}</h5>
-          <h5>Overtime Losses: {{ team.overtime_losses }}</h5>
-          <h5>Shootout Wins: {{ team.shootout_wins }}</h5>
-          <h5>Shootout Losses: {{ team.shootout_losses }}</h5>
+    <!-- Breadcrumb Area -->
+    <section class="breadcrumb-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="breadcrumb-box">
+              <ul class="list-unstyled list-inline">
+                <li class="list-inline-item">
+                  <!-- Dropdown select for season -->
+                  <select class="form-control" @change="changeSeasons($event)">
+                    <option value="" selected disabled>Choose Season</option>
+                    <option v-for="season in seasons">{{ season }}</option>
+                  </select>
+                  <!-- Free version of API only allows for selection of two seasons.  If I ever go with paid version then will make changes to add playoffs and more historical season data -->
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <!-- NFL is the only league that allows for a -->
-        <div v-else-if="team.league == 'NFL'">
-          <h5>Games Tied: {{ team.games_tied }}</h5>
-        </div>
-        <h5>Conference Rank: {{ team.conference_rank }}</h5>
-        <h5>Division Rank: {{ team.division_rank }}</h5>
       </div>
-    </div>
-    <br />
+    </section>
+    <!-- End Breadcrumb Area -->
 
-    <div v-for="article in team.articles">
-      <img :src="article.url_image" alt="" />
-      <!-- <h4>description: {{ article.description }}</h4> -->
-      <h6>{{ article.source_name }}</h6>
-      <h4>{{ article.title }}</h4>
-      <a :href="`${article.url}`" target="_blank">{{ article.url }}</a>
+    <!-- Category Area -->
+    <section class="category1-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="category-box-lg">
+                  <img :src="team.official_logo" alt="" class="img-fluid" />
+                  <h6>
+                    <a href="">{{ team.name }}</a>
+                  </h6>
+                  <ul class="list-unstyled list-inline tag-dat">
+                    <li class="list-inline-item">
+                      Season: {{ selectedSeason }}
+                    </li>
+                    <li class="list-inline-item">February 11, 2019</li>
+                  </ul>
+                  <p>
+                    <!-- Added a NCAAF if loop for future NCAA implementation -->
+                    <div v-if="team.league == 'NCAA'">
+                      <h5>Conference: {{ team.conference_name }}</h5>
+                      <h5>Games Played: {{ team.games_played }}</h5>
+                      <h5>Games Won: {{ team.games_won }}</h5>
+                      <h5>Games Lost: {{ team.games_lost }}</h5>
+                      <h5>Conference Wins: {{ team.conference_wins }}</h5>
+                      <h5>Conference Losses: {{ team.conference }}</h5>
+                    </div>
+                    <div v-else>
+                      <h5>Conference: {{ team.conference_name }}</h5>
+                      <h5>Division: {{ team.division_name }}</h5>
+                      <h5>Home Venue: {{ team.team_venue }}</h5>
+                      <h5>Games Played: {{ team.games_played }}</h5>
+                      <h5>Games Won: {{ team.games_won }}</h5>
+                      <h5>Games Lost: {{ team.games_lost }}</h5>
+                      <!-- NHL had unique stats -->
+                      <div v-if="team.league == 'NHL'">
+                        <h5>Overtime Wins: {{ team.overtime_wins }}</h5>
+                        <h5>Overtime Losses: {{ team.overtime_losses }}</h5>
+                        <h5>Shootout Wins: {{ team.shootout_wins }}</h5>
+                        <h5>Shootout Losses: {{ team.shootout_losses }}</h5>
+                      </div>
+                      <!-- NFL is the only league that allows for a -->
+                      <div v-else-if="team.league == 'NFL'">
+                        <h5>Games Tied: {{ team.games_tied }}</h5>
+                      </div>
+                      <h5>Conference Rank: {{ team.conference_rank }}</h5>
+                      <h5>Division Rank: {{ team.division_rank }}</h5>
+                    </div>
+                  </p>
+                  <ul class="list-unstyled list-inline red-com">
+                    <li class="list-inline-item">
+                      <a href="#"
+                        >Read More <i class="fa fa-angle-right"></i
+                      ></a>
+                    </li>
+                    <li class="list-inline-item">
+                      <a href=""><i class="fa fa-commenting-o"></i> (09)</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
 
-      <p></p>
-      <br />
-    </div>
+              <div v-for="article in team.articles" class="col-md-12">
+                <div class="category-box2 d-flex">
+                  <div class="cat-img">
+                    <a href="#"
+                      ><img
+                        id="team-show-article-img"
+                        :src="article.url_image"
+                        alt=""
+                    /></a>
+                  </div>
+                  <div class="img-content">
+                    <h6>
+                      <a href="">{{ article.title }}</a>
+                    </h6>
+                    <ul class="list-unstyled list-inline tag-dat">
+                      <li class="list-inline-item">
+                        {{ article.source_name }}
+                      </li>
+                      <li class="list-inline-item">February 11, 2019</li>
+                    </ul>
+                    <p>{{ article.description }}</p>
+
+                    <a :href="`${article.url}`" target="_blank"
+                      >Read More <i class="fa fa-angle-right"></i
+                    ></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Category Area -->
   </div>
 </template>
+
+<style>
+#team-show-article-img {
+  width: 260px;
+  height: 169px;
+}
+</style>
+
 <script>
 import axios from "axios";
 
@@ -85,7 +152,6 @@ export default {
         console.log(response.data);
         this.team = response.data;
       });
-
   },
   methods: {
     changeSeasons(event) {
